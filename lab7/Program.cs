3e16069a4de28047b05775b42e833d39c7257ffb
neDmitry-sleep <verygadverygad@gmail.com>
2025-04-21 12:25:50 +0700

    Add DBConnection

--- lab7/Program.cs
+++ lab7/Program.cs
@@ -1,3 +1,7 @@
+using lab7.Model;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Добавьте перед builder.Build()
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
